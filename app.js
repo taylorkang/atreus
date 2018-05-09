290ad8692e54a9865a2f4d970b0e278bd91d6797
Taylor Kang <tkang.cs@gmail.com>
2018-05-09 15:15:42 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,74 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, 
+    function () {
+        console.log(`${server.name} listening to ${server.url}`);
+    });
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword
+});
+
+// Listen for messages from users
+server.post('/api/messages', connector.listen());
+
+var inMemoryStorage = new builder.MemoryBotStorage();
+
+// This bot ensures user's profile is up to date.
+var bot = new builder.UniversalBot(connector, [
+    function (session) {
+        session.send("Hi! I'm Atreus, the Kang family assistant.");
+        session.beginDialog('ensureProfile', session.userData.profile);
+    },
+    function (session, results) {
+        session.userData.profile = results.response; // Save user profile.
+        session.send(`Hello ${session.userData.profile.name}! I love ${session.userData.profile.company}!`);
+    }
+]).set('storage', inMemoryStorage); // Register in-memory storage 
+
+bot.dialog('ensureProfile', [
+    function (session, args, next) {
+        session.dialogData.profile = args || {}; // Set the profile or create the object.
+        if (!session.dialogData.profile.name) {
+            builder.Prompts.text(session, "What's your name?");
+        } else {
+            next(); // Skip if we already have this info.
+        }
+    },
+    function (session, results, next) {
+        if (results.response) {
+            // Save user's name if we asked for it.
+            session.dialogData.profile.name = results.response;
+        }
+        if (!session.dialogData.profile.company) {
+            builder.Prompts.text(session, "What company do you work for?");
+        } else {
+            next(); // Skip if we already have this info.
+        }
+    },
+    function (session, results) {
+        if (results.response) {
+            // Save company name if we asked for it.
+            session.dialogData.profile.company = results.response;
+        }
+        session.endDialogWithResult({ response: session.dialogData.profile });
+    }
+]);
+
+// // This is a dinner reservation bot that uses a waterfall technique to prompt users for input.
+// var bot = new builder.UniversalBot(connector, [
+//     function (session) {
+//         session.send("Hi! I'm Atreus, the Kang family assistant.");
+//         builder.Prompts.text(session, "Who am I speaking with?");
+//     },
+//     function (session, results) {
+//         session.dialogData.user = results.response;
+//         builder.Prompts.text(session, `Nice to see you, ${session.dialogData.user}.`);
+//         session.endDialog();
+//     }
+// ]).set('storage', inMemoryStorage); // Register in-memory storage 
